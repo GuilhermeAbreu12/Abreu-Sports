33400629c41744d007ced5ac6907e7969dd0587b
Guilherme Abreu <115832754+GuilhermeAbreu12@users.noreply.github.com>
2025-05-12 15:42:02 -0300

    Inclusão da estrutura inicial
    Adicionado:
    - index.php (html básico da página);
    - favicon (bola-de-futebol.png);
    - style.css (css inicial com o estilo dos cards e da imagem de fundo);
    - framework (bootstrap 5.3).

---
+++ Abreu Sports/index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Abreu Sports</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+    <link rel="shortcut icon" href="imgs/bola-de-futebol.png" type="image/x-icon">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <section id="container-cards" class="container">
+        <div class="row">
+            <div class="card col-12 col-sm-6 col-md-3 col-lg-2">
+                <div class="card-head">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2BoHcUiAASk8aygWzHBF-hrpyuSPxfEIqtw&s" alt="">
+                </div>
+                <div class="card-body">
+                    <button class="button">Acessar</button>
+                </div>
+            </div>
+            <div class="card col-12 col-sm-6 col-md-3 col-lg-2">
+                <div class="card-head">
+                </div>
+                <div class="card-body">
+                    <button class="button">Acessar</button>
+                </div>
+            </div>
+            <div class="card col-12 col-sm-6 col-md-3 col-lg-2">
+                <div class="card-head">
+                </div>
+                <div class="card-body">
+                    <button class="button">Acessar</button>
+                </div>
+            </div>
+            <div class="card col-12 col-sm-6 col-md-3 col-lg-2">
+                <div class="card-head">
+                </div>
+                <div class="card-body">
+                    <button class="button">Acessar</button>
+                </div>
+            </div>
+            <div class="card col-12 col-sm-6 col-md-3 col-lg-2">
+                <div class="card-head">
+                </div>
+                <div class="card-body">
+                    <button class="button">Acessar</button>
+                </div>
+            </div>
+            <div class="card col-12 col-sm-6 col-md-3 col-lg-2">
+                <div class="card-head">
+                </div>
+                <div class="card-body">
+                    <button class="button">Acessar</button>
+                </div>
+            </div>
+            <div class="card col-12 col-sm-6 col-md-3 col-lg-2">
+                <div class="card-head">
+                </div>
+                <div class="card-body">
+                    <button class="button">Acessar</button>
+                </div>
+            </div>
+            <div class="card col-12 col-sm-6 col-md-3 col-lg-2">
+                <div class="card-head">
+                </div>
+                <div class="card-body">
+                    <button class="button">Acessar</button>
+                </div>
+            </div>
+        </div>
+    </section>
+    
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.min.js" integrity="sha384-RuyvpeZCxMJCqVUGFI0Do1mQrods/hhxYlcVfGPOfQtPJh0JCw12tUAZ/Mv10S7D" crossorigin="anonymous"></script>
+    <script src="js/script.js" defer></script>
+</body>
+</html>
