c69c4514f661a8d8550023da3b2b1563318b176b
Guilherme Abreu <guilhermesabreu08@gmail.com>
2025-05-21 08:02:20 -0300

    Personalização dos cards
    - Cards com as logotipos dos seus times;
    - Background-color com as respectivas cores.

--- index.php
+++ index.php
@@ -9,44 +9,48 @@
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <h1 id='title'>Abreu Sports</h1>
+    <h1 id='title'>Abreu <span>Sports</span></h1>
     <p id='subtitle'>Escolha seu time favorito</p>
     <section id="container-cards" class="container">
         <div class="row">
-            <div class="card col-12 col-sm-6 col-md-3 col-lg-2" id="card-spfc">
+            <div class="card col-12 col-sm-6 col-md-3 col-lg-2" id="team-card1">
                 <div class="card-head">
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2BoHcUiAASk8aygWzHBF-hrpyuSPxfEIqtw&s" alt="">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Brasao_do_Sao_Paulo_Futebol_Clube.svg/2054px-Brasao_do_Sao_Paulo_Futebol_Clube.svg.png" id="team-logo1" alt="Logotipo do São Paulo">
                 </div>
                 <div class="card-body">
-                    <button class="button">Acessar</button>
+                    <a href="" class="btn-team">Acessar</a>
                 </div>
             </div>
-            <div class="card col-12 col-sm-6 col-md-3 col-lg-2">
+            <div class="card col-12 col-sm-6 col-md-3 col-lg-2" id="team-card2">
                 <div class="card-head">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Palmeiras_logo.svg/1024px-Palmeiras_logo.svg.png" id="team-logo2" alt="Logotipo do Palmeiras">
                 </div>
                 <div class="card-body">
-                    <button class="button">Acessar</button>
+                    <a href="" class="btn-team">Acessar</a>
                 </div>
             </div>
-            <div class="card col-12 col-sm-6 col-md-3 col-lg-2">
+            <div class="card col-12 col-sm-6 col-md-3 col-lg-2" id="team-card3">
                 <div class="card-head">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Santos_logo.svg/2090px-Santos_logo.svg.png" id="team-logo3" alt="Logotipo do Santos">
                 </div>
                 <div class="card-body">
-                    <button class="button">Acessar</button>
+                    <a href="" class="btn-team">Acessar</a>
                 </div>
             </div>
-            <div class="card col-12 col-sm-6 col-md-3 col-lg-2">
+            <div class="card col-12 col-sm-6 col-md-3 col-lg-2" id="team-card4">
                 <div class="card-head">
+                    <img src="https://images.seeklogo.com/logo-png/3/2/clube-de-regatas-do-flamengo-logo-png_seeklogo-32416.png" id="team-logo4" alt="Logotipo do Flamengo">
                 </div>
                 <div class="card-body">
-                    <button class="button">Acessar</button>
+                    <a href="" class="btn-team">Acessar</a>
                 </div>
             </div>
-            <div class="card col-12 col-sm-6 col-md-3 col-lg-2">
+            <div class="card col-12 col-sm-6 col-md-3 col-lg-2" id="team-card5">
                 <div class="card-head">
+                    <img src="https://images.seeklogo.com/logo-png/12/2/sc-corinthians-paulista-logo-png_seeklogo-123174.png" id="team-logo5" alt="Logotipo do Corinthians">
                 </div>
                 <div class="card-body">
-                    <button class="button">Acessar</button>
+                    <a href="" class="btn-team">Acessar</a>
                 </div>
             </div>
         </div>
