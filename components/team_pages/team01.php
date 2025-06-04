e976ed9aa58ed07be02a3ffb2580cecd0715eddd
Guilherme Abreu <guilhermesabreu08@gmail.com>
2025-06-04 07:47:22 -0300

    Feat: aside no team01.php

--- components/team_pages/team01.php
+++ components/team_pages/team01.php
@@ -1,5 +1,13 @@
 <section class="team-section">
-    <div class="team-section__team-icon" id="team-section1">
-        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Brasao_do_Sao_Paulo_Futebol_Clube.svg/2054px-Brasao_do_Sao_Paulo_Futebol_Clube.svg.png" alt="logo do São Paulo FC" class="img-fluid" id="team-icon1">
+    <aside>
+        <div class="team-section__team-icon" id="team-section1">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Brasao_do_Sao_Paulo_Futebol_Clube.svg/2054px-Brasao_do_Sao_Paulo_Futebol_Clube.svg.png" alt="logo do São Paulo FC" class="img-fluid" id="team-icon1">
+        </div>
+    </aside>
+    <div>
+        <h1>São Paulo FC</h1>
     </div>
+    <aside>
+
+    </aside>
 </section>
