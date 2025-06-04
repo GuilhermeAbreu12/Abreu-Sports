2c47309d22823facbfbbc408c5f91970c4f9b570
Guilherme Abreu <guilhermesabreu08@gmail.com>
2025-06-04 14:08:00 -0300

    design: separar a .team-section em três partes

--- components/team_pages/team01.php
+++ components/team_pages/team01.php
@@ -1,13 +1,14 @@
-<section class="team-section">
-    <aside>
+<section class="team-section"> <!-- Container branco -->
+    <aside id="team-section__left"> <!-- Aside esquerdo-->
         <div class="team-section__team-icon" id="team-section1">
             <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Brasao_do_Sao_Paulo_Futebol_Clube.svg/2054px-Brasao_do_Sao_Paulo_Futebol_Clube.svg.png" alt="logo do São Paulo FC" class="img-fluid" id="team-icon1">
         </div>
     </aside>
-    <div>
+    <div id="team-section__center">
         <h1>São Paulo FC</h1>
     </div>
-    <aside>
+    <aside id="team-section__right">
 
     </aside>
 </section>
+
